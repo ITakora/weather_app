55ce71dc75dce755bd7b36af61ee16a32c52367f
Aris <ahmadrizkydwiyanto@gmail.com>
2023-08-26 00:06:08 +0700

    adding some error handling

--- lib/data/api/weather_api.dart
+++ lib/data/api/weather_api.dart
@@ -6,13 +6,17 @@ import 'package:http/http.dart' as http;
 
 class WeatherApi {
   Future<WeatherModel> getApi(String location) async {
-    final Uri url = Uri.parse(
-        'https://api.weatherapi.com/v1/forecast.json?key=a85701f9571843d18fa152930233107&q=$location&days=6');
+    try {
+      final Uri url = Uri.parse(
+          'https://api.weatherapi.com/v1/forecast.json?key=a85701f9571843d18fa152930233107&q=$location&days=6');
 
-    final respone = await http.get(url);
-    final Map<String, dynamic> data = (jsonDecode(respone.body));
-    final dataApi = WeatherModel.fromJson(data);
-    return dataApi;
+      final respone = await http.get(url);
+      final Map<String, dynamic> data = (jsonDecode(respone.body));
+      final dataApi = WeatherModel.fromJson(data);
+      return dataApi;
+    } catch (e) {}
+
+    return getApi(location);
   }
 }
 
